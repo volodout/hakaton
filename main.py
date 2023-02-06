fa1d6f05a2358c1cc20aa862186cbd8aaf617e3d
volodin <slavavolodin868@gmail.com>
2023-02-06 18:32:01 +0500

    modes_4

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from pprint import pprint
 
 import pygame as pg
-from random import choice
+from random import choice, sample
+import time
+
+COLORS = ['red', 'orange', 'yellow', 'Cyan', 'blue', 'purple', 'Magenta', 'Lime']
 
 
 class XmasTree:
@@ -18,55 +21,74 @@ class XmasTree:
             pg.draw.polygon(surface, '#007700', area)
 
         pg.draw.rect(surface, 'brown', ((370, 600), (50, 100)))
-        pg.draw.polygon(surface, 'yellow', ([400, 20], [440, 130], [340, 60], [460, 60], [360, 130]))
+        pg.draw.polygon(surface, 'red', ([400, 20], [440, 130], [340, 60], [460, 60], [360, 130]))
+        pg.draw.polygon(surface, 'red', ([386, 60], [414, 60], [424, 87], [400, 103], [375, 87]))
         # pprint(self.areas)
 
     def draw_toys(self, surface):
         self.toys_coords = []
         with open('points.txt', 'r') as f:
             for i in f.readlines():
-                self.toys_coords.append(tuple(map(int, i.split(', '))))
+                self.toys_coords.append(tuple(map(int, i.split())))
         for i in self.toys_coords:
             pg.draw.circle(surface, 'white', i, 7)
 
     def choice_mode(self):
-        global mode
-        mode = int(input('Выберете режим работы (от 1 до (?)): '))
+        global garland_mode
+        garland_mode = int(input('Выберете режим работы (от 1 до (?)): '))
 
-    def change_colors(self):
-        global mode, tick
-        colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
-
+    def change_colors(self, mode, ticks):
         if mode == 1:
             for i in self.toys_coords:
-                pg.draw.circle(screen, choice(colors), i, 7)
+                if ticks % 2 == 0:
+                    pg.draw.circle(screen, choice(COLORS), i, 7)
 
         elif mode == 2:
-            color = choice(colors)
+            color = choice(COLORS)
             for i in self.toys_coords:
-                if tick == 1:
+                if ticks % 5 == 0:
                     pg.draw.circle(screen, color, i, 7)
 
         elif mode == 3:
-            n = 0
-            color = choice(colors)
-            while n < 80:
-                for i in self.toys_coords:
-                    if tick % 5 == 0:
-                        for j in range(n):
-                            pg.draw.circle(screen, color, i, 7)
-                    n = n % 80 + 1
+            global color3, num
+            for i in range(num):
+                pg.draw.circle(screen, color3, self.toys_coords[i], 7)
+            pg.draw.circle(screen, 'yellow', self.toys_coords[num-1], 7, 1)
+            num += 1
+            if num > 125:
+                color3 = choice(COLORS)
+                num = 1
 
-    # def points.txt(self, pos):
-    #     with open('points.txt.txt', 'a+') as f:
-    #         f.write(str(pos) + '\n')
+        elif mode == 4:
+            global c1, c2, n4
+            if ticks % 12 == 0:
+                for i in range(0, len(self.toys_coords), 2):
+                    pg.draw.circle(screen, c1, self.toys_coords[i], 7)
+                for i in range(1, len(self.toys_coords), 2):
+                    pg.draw.circle(screen, c2, self.toys_coords[i], 7)
+                c1, c2 = c2, c1
+                n4 += 1
+            if n4 == 4:
+                c1, c2 = sample(COLORS, k=2)
+                n4 = 0
+
+
+    # def points(self, pos):
+    #     with open('points.txt', 'a+') as f:
+    #         f.write(str(pos[0]) + ' ' + str(pos[1]) + '\n')
     #         pg.draw.circle(screen, 'red', pos, 7)
 
 
 if __name__ == '__main__':
     tree = XmasTree()
-    mode = None
+    garland_mode = None
     tree.choice_mode()
+    if garland_mode == 3:
+        color3 = choice(COLORS)
+        num = 1
+    if garland_mode == 4:
+        c1, c2 = sample(COLORS, k=2)
+        n4 = 0
 
     pg.init()
     size = 800, 800
@@ -83,12 +105,12 @@ if __name__ == '__main__':
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
-            # if event.type == pg.MOUSEMOTION:
-            #     print(event.pos)
+            if event.type == pg.MOUSEMOTION:
+                print(event.pos)
             # if event.type == pg.MOUSEBUTTONDOWN:
-            #     tree.points.txt(event.pos)
-        tree.change_colors()
-        tick = tick % 10 + 1
-        print(tick)
+            #     tree.points(event.pos)
+        tree.change_colors(garland_mode, tick)
+        tick = tick % 50 + 1
+        # print(tick)
         pg.display.flip()
-        clock.tick(5)
+        clock.tick(15)
